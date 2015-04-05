414cabe871eb621372181885603ecb87f08846ea
islaytitans <jonthan.m.robbins@gmail.com>
2015-04-05 18:28:01 +0100

    Added Unlockable Items gutter

---
+++ MasterKey/Gutters/UnlockableItems.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Data.Items;
+using Sitecore.Globalization;
+using Sitecore.Shell.Applications.ContentEditor.Gutters;
+
+namespace MasterKey.Gutters
+{
+    public class UnlockableItems : GutterRenderer
+    {
+        protected override GutterIconDescriptor GetIconDescriptor(Item item)
+        {
+            if (!item.Locking.IsLocked() || !item.Access.CanWrite() ||  !item.Access.CanWriteLanguage())
+                return (GutterIconDescriptor)null;
+
+            var gutterIconDescriptor = new GutterIconDescriptor
+            {
+                Icon = "Network/32x32/key1.png",
+                Tooltip = Translate.Text("Locked by") + " " + item.Locking.GetOwnerWithoutDomain()
+            };
+
+            if (item.Locking.CanUnlock() && item.Access.CanWrite() && item.Access.CanWriteLanguage() &&
+                !item.Appearance.ReadOnly)
+                gutterIconDescriptor.Click = "MasterKey:UnlockItem(id=" + (object)item.ID + ")";
+            
+            return gutterIconDescriptor;
+        }
+    }
+}
